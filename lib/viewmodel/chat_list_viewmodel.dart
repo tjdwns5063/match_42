2142cc4f5c4afa67dd7277586f183e92c60539d0
seongjunkim <tjdwns5063@naver.com>
2023-08-27 17:14:54 +0900

    Feat: 채팅방 변경시마다 ui 갱신하는 로직 추가
    스트림에서 변경을 구독하는 listen 메서드 구현

--- lib/viewmodel/chat_list_viewmodel.dart
+++ lib/viewmodel/chat_list_viewmodel.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:match_42/data/chat_room.dart';
 import 'package:match_42/service/chat_service.dart';
@@ -17,6 +18,18 @@ class ChatListViewModel extends ChangeNotifier {
   void _init() {
     _chatService.getAllChatRoom().then((List<ChatRoom> rooms) {
       _rooms = rooms;
+      notifyListeners();
+    });
+  }
+
+  void listen() {
+    final Stream<QuerySnapshot<ChatRoom>> stream =
+        _chatService.roomRef.snapshots();
+    stream.listen((event) {
+      List<ChatRoom> newRooms = [
+        for (QueryDocumentSnapshot<ChatRoom> doc in event.docs) doc.data()
+      ];
+      _rooms = newRooms;
       notifyListeners();
     });
   }
