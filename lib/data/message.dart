745c378c19e383244327991867e2e87000ae7107
seongjunkim <tjdwns5063@naver.com>
2023-08-27 17:11:25 +0900

    Feat: Timestamp를 Format 하는 확장함수 구현
    '오후 12:00' 형식으로 Timestamp를 포매팅하는 확장함수를 구현

--- lib/data/message.dart
+++ lib/data/message.dart
@@ -45,3 +45,14 @@ class Message {
     return 'sender: $sender message: $message date: ${date.toDate()}';
   }
 }
+
+extension FormatDate on Timestamp {
+  String toFormatString() {
+    DateTime date = toDate();
+
+    String prefix = date.hour >= 12 ? '오후' : '오전';
+    String hour = date.hour > 12 ? '${date.hour - 12}' : '${date.hour}';
+    String min = date.minute < 10 ? '0${date.minute}' : date.minute.toString();
+    return '$prefix $hour:$min';
+  }
+}
