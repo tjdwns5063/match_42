b6e1965113d96d8d9d659a1a5cdce5b8ba261492
seongjunkim <tjdwns5063@naver.com>
2023-09-14 13:48:21 +0900

    Refactor: ChatViewModelTest 중복되는 초기화 로직 정리
    각 테스트 함수에서 중복되는 초기화 로직을 ChatViewModelTest 클래스를
    만들고 초기화 메서드를 생성하여 중복을 제거함

--- integration_test/chat_viewmodel_test.dart
+++ integration_test/chat_viewmodel_test.dart
@@ -13,90 +13,78 @@ import 'FirebaseSetter.dart';
 @GenerateNiceMocks([MockSpec<UserService>()])
 import 'chat_viewmodel_test.mocks.dart';
 
-Future<void> sendMessageTest() async {
-  User me = User(id: 1, nickname: '', intra: 'seongjki', profile: '');
-  MockUserService userService = MockUserService();
-  ChatService chatService = ChatService.instance;
-  ChatRoom chatRoom = ChatRoomCreator.create();
-  final ref = await chatService.addChatRoom(chatRoom);
+class ChatViewModelTest {
+  late User me;
+  late MockUserService userService;
+  late ChatService chatService;
+  late ChatRoom chatRoom;
+  late ChatViewModel chatViewModel;
 
-  ChatViewModel chatViewModel = ChatViewModel(
-      roomId: ref.id,
-      user: me,
-      token: 'token',
-      chatService: chatService,
-      userService: userService);
+  Future<void> init() async {
+    me = User(id: 1, nickname: '', intra: 'seongjki', profile: '');
+    userService = MockUserService();
+    chatService = ChatService.instance;
+    chatRoom = ChatRoomCreator.create();
+    final ref = await chatService.addChatRoom(chatRoom);
 
-  await chatViewModel.send(me, TextEditingController(text: 'hello'));
+    chatViewModel = ChatViewModel(
+        roomId: ref.id,
+        user: me,
+        token: 'token',
+        chatService: chatService,
+        userService: userService);
+  }
 
-  expect(chatViewModel.messages[0].message, 'hello');
-  verify(userService.sendNotification(2, 'seongjki: hello', 'token'));
+  Future<void> sendMessageTest() async {
+    await chatViewModel.send(me, TextEditingController(text: 'hello'));
 
-  chatViewModel.dispose();
-}
+    expect(chatViewModel.messages[0].message, 'hello');
+    verify(userService.sendNotification(2, 'seongjki: hello', 'token'));
 
-Future<void> whenRemainTimeZeroAllUserDecideOpenIdTest() async {
-  User me = User(id: 1, nickname: '', intra: 'seongjki', profile: '');
-  MockUserService userService = MockUserService();
-  ChatService chatService = ChatService.instance;
-  ChatRoom chatRoom = ChatRoomCreator.create();
-  final ref = await chatService.addChatRoom(chatRoom);
+    chatViewModel.dispose();
+  }
 
-  when(userService.getUserIntraNames([1, 2], 'token'))
-      .thenAnswer((_) => Future.value(['seongjki', 'jiheekan']));
+  Future<void> whenRemainTimeZeroAllUserDecideOpenIdTest() async {
+    when(userService.getUserIntraNames([1, 2], 'token'))
+        .thenAnswer((_) => Future.value(['seongjki', 'jiheekan']));
 
-  ChatViewModel chatViewModel = ChatViewModel(
-      roomId: ref.id,
-      user: me,
-      token: 'token',
-      chatService: chatService,
-      userService: userService);
+    chatViewModel.chatRoom.updateIsOpen(2);
 
-  chatViewModel.chatRoom.updateIsOpen(2);
+    await chatViewModel.updateOpenResult();
 
-  await chatViewModel.updateOpenResult();
+    expect(chatViewModel.chatRoom.isEveryOpened(), true);
+    verifyInOrder([
+      userService.getUserIntraNames([1, 2], 'token'),
+      userService.sendNotification(1, 'seongjki, jiheekan 님이 매치되었습니다', 'token'),
+      userService.sendNotification(2, 'seongjki, jiheekan 님이 매치되었습니다', 'token')
+    ]);
 
-  expect(chatViewModel.chatRoom.isEveryOpened(), true);
-  verifyInOrder([
-    userService.getUserIntraNames([1, 2], 'token'),
-    userService.sendNotification(1, 'seongjki, jiheekan 님이 매치되었습니다', 'token'),
-    userService.sendNotification(2, 'seongjki, jiheekan 님이 매치되었습니다', 'token')
-  ]);
+    chatViewModel.dispose();
+  }
 
-  chatViewModel.dispose();
-}
+  Future<void> conversationTopicRecommendationTest() async {
+    await chatViewModel.makeTopic();
 
-Future<void> conversationTopicRecommendationTest() async {
-  User me = User(id: 1, nickname: '', intra: 'seongjki', profile: '');
-  MockUserService userService = MockUserService();
-  ChatService chatService = ChatService.instance;
-  ChatRoom chatRoom = ChatRoomCreator.create();
-  final ref = await chatService.addChatRoom(chatRoom);
-  ChatViewModel chatViewModel = ChatViewModel(
-      roomId: ref.id,
-      user: me,
-      token: 'token',
-      chatService: chatService,
-      userService: userService);
+    expect(topics.contains(chatViewModel.messages[0].message), true);
 
-  await chatViewModel.makeTopic();
-
-  expect(topics.contains(chatViewModel.messages[0].message), true);
-
-  chatViewModel.dispose();
+    chatViewModel.dispose();
+  }
 }
 
 Future<void> main() async {
   await FirebaseSetter.init();
+  ChatViewModelTest chatViewModelTest = ChatViewModelTest();
 
   setUp(() async {
     await FirebaseSetter.deleteFirestore();
+    await chatViewModelTest.init();
   });
 
   group('chat viewmodel test', () {
-    test('메세지 전송 기능 테스트', () => sendMessageTest());
+    test('메세지 전송 기능 테스트', () => chatViewModelTest.sendMessageTest());
     test('채팅 시간이 끝난 후, 모두 아이디 공개를 결정한 경우를 테스트',
-        () => whenRemainTimeZeroAllUserDecideOpenIdTest());
-    test('대화 주제 추천 기능 테스트', () => conversationTopicRecommendationTest());
+        () => chatViewModelTest.whenRemainTimeZeroAllUserDecideOpenIdTest());
+    test('대화 주제 추천 기능 테스트',
+        () => chatViewModelTest.conversationTopicRecommendationTest());
   });
 }
